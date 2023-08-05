a3412a37b9baf3fd950b63a5432ffff2f7cdc260
ogbumichael <michaelemekaogbu@gmail.com>
2023-08-05 00:34:55 -0500

    skill section still under construction

--- main.js
+++ main.js
@@ -1,7 +1,6 @@
 // ===========
 // NAVIGATION
 // ===========
-
 const mainNav = document.querySelector(".NAV_CONTAINER");
 const navTimes = document.querySelector("#navTimes");
 const navBars = document.querySelector("#navBar");
@@ -36,11 +35,36 @@ window.addEventListener("scroll", () => {
 // SKILL SECTION
 // ===============
 const skillbtns = document.querySelectorAll(".skillHead");
-const skillOne = document.querySelector("#skillHeadOne");
+const skillOne = document.querySelector(".skillHeadOne");
+const skillTwo = document.querySelector(".skillHeadTwo");
+const skillThree = document.querySelector(".skillHeadThree");
+const skillFour = document.querySelector(".skillHeadFour");
+// console.log(skillContainer.children);
+
+function showNext () {
+    const skillShow = document.querySelector('.skillShow');
+    // const skillContainer = document.querySelector('.skill_right');
+    const current = skillShow.nextElementSibling;
+    console.log(current);
+    skillShow.classList.remove('skillShow')
+    if(current){
+        current.classList.add('skillShow')
+    }else{
+        skillShow.lastElementChild
+    }
+}
 
 skillbtns.forEach((skilllinks) => {
     skilllinks.addEventListener("click", (e) => {
-        e.preventDefault();
-        console.log(123);
+        e.currentTarget;
+        if(skilllinks.classList.contains('skillHeadOne')){
+            showNext();
+        }else if(skilllinks.classList.contains('skillHeadTwo')){
+            showNext();
+        }else if(skilllinks.classList.contains('skillHeadThree')){
+            showNext();
+        }else if(skilllinks.classList.contains('skillHeadFour')){
+            showNext();
+        }
     })
 })
